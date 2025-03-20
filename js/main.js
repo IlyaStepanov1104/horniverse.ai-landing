9dbeada9fa9a984644e562172769ae81b625290e
IlyaStepanov1104 <ilyahtml@gmai.com>
2025-03-20 11:11:21 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
     const $burgerMenu = $('.burger-menu'),
-    $burgerOpen = $('.burger-menu-icon'),
-    $burgerClose = $('.burger-close');
+        $burgerOpen = $('.burger-menu-icon'),
+        $burgerClose = $('.burger-close');
 
     $burgerMenu.hide();
 
@@ -9,20 +9,37 @@ $(document).ready(function() {
     $burgerClose.on('click', () => $burgerMenu.hide());
 
 
-
-    $('[data-copy]').click(function() {
+    $('[data-copy]').click(function () {
         var copyText = $(this).data('copy');
         const buttonElement = $(this);
-        navigator.clipboard.writeText(copyText).then(function() {
+        navigator.clipboard.writeText(copyText).then(function () {
             const copiedElement = buttonElement.parent().children('.copied');
             copiedElement.show();
             setTimeout(() => copiedElement.hide(), 2000);
-        }).catch(function(err) {
+        }).catch(function (err) {
             alert('Failed to copy text: ', err);
         });
     });
 
-    $('.leaderboard-icon').click(function() {
+    $('.leaderboard-icon').click(function () {
         $(this).toggleClass('active');
     });
+
+    const attemptTypes = ['bronze', 'silver', 'gold'];
+    attemptTypes.forEach((type) => $.ajax({
+        url: 'https://horniverse.ai/game/get-config',  // Адрес эндпоинта
+        method: 'GET',            // Метод запроса
+        data: {key: `${type}_attempts`},  // Параметры запроса
+        success: function (response) {
+            if (response && response.value) {
+                $(`.js-${type}-attempts`).text(response.value);
+            } else {
+                console.error('Ошибка: Не удалось получить данные');
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error('Ошибка запроса: ' + error);
+        }
+    }));
 });
+
